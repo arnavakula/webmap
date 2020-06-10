58d418a2660e38b9556b81f198168e1bb0c881b3
Arnav Akula <akula.arnav@gmail.com>
2020-06-09 19:41:52 -0700

    Volcano markers iterated from dataframe

--- web-map.py
+++ web-map.py
@@ -1,11 +1,16 @@
-import folium
+import folium, pandas
 
 map = folium.Map(location = [38.2, -99.1], zoom_start = 6, tiles = "Stamen Terrain") #lat, lon
 fg = folium.FeatureGroup(name = 'my-map')
+dfv = pandas.read_csv("volcanoes.txt")
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    fg.add_child(folium.Marker(location = coordinates, popup = "Testing marker", icon = folium.Icon(color = 'red')))
+
+for i in range(62):
+    fg.add_child(folium.Marker(location = [float(dfv.loc[i, "LAT"]), dfv.loc[i, "LON"]], popup = dfv.loc[i, "NAME"], icon = folium.Icon(color = 'cadetblue')))
+
+
 
 map.add_child(fg)
 map.save("Web-map.html")
+
 
