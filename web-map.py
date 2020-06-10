a24a2fa04836b0ee0ca5d8824f76f80c4f8f2d68
Arnav Akula <akula.arnav@gmail.com>
2020-06-09 18:49:53 -0700

    Iterated to add markers

--- web-map.py
+++ web-map.py
@@ -1,6 +1,11 @@
 import folium
 
-map = folium.Map(location = [38.2, -99.1], tiles = "Stamen Terrain") #lat, lon
+map = folium.Map(location = [38.2, -99.1], zoom_start = 6, tiles = "Stamen Terrain") #lat, lon
+fg = folium.FeatureGroup(name = 'my-map')
 
+for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+    fg.add_child(folium.Marker(location = coordinates, popup = "Testing marker", icon = folium.Icon(color = 'red')))
+
+map.add_child(fg)
 map.save("Web-map.html")
 
